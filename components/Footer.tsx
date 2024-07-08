2303e36f4b15b28fb9ae9d5f9e0a69fb0a683460
chip <114098392+dchipp@users.noreply.github.com>
2024-07-08 20:36:52 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ const phone: string = "+39 345 726 4487"
 const email: string = "patrickmascia42@gmail.com"
 const facebook: string = "https://www.facebook.com/patrick.mascia.42"
 const linkedin: string = "https://www.linkedin.com/in/patrickfmascia/"
+const github: string = "https://github.com/dchipp"
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -14,6 +15,7 @@ export default function Footer() {
         <a href={`mailto:${email}`}>Email</a>
         <a href={facebook}>Facebook</a>
         <a href={linkedin}>LinkedIn</a>
+        <a href={github}>GitHub</a>
       </div>
       <div className="copyright text-xs text-secondary">
         &copy; Copyright {year} Patrick Francesco Mascia, All Rights Reserved
